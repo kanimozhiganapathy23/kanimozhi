9ffa4d2d26d3da6963e20b2ce1d09b945b133b23
kanimozhiganapathy23 <34593364+kanimozhiganapathy23@users.noreply.github.com>
2017-12-16 07:46:34 -0800

    Create prog5.py

---
+++ prog5.py
@@ -0,0 +1,13 @@
+a=int(raw_input("enter a"))
+print a
+b=int(raw_input("enter b"))
+print b
+c=int(raw_input("enter c"))
+print c
+if(a>b and a>c):
+	print("a is greater")
+elif(b>a and b>c)
+	print("b is greater")
+else:
+	print("c is greater")
+
