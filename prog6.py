44a676b63e1dfe17127d1daa0c074c2ad1cb6a53
kanimozhiganapathy23 <34593364+kanimozhiganapathy23@users.noreply.github.com>
2017-12-16 07:51:36 -0800

    Create prog6.py

---
+++ prog6.py
@@ -0,0 +1,9 @@
+x=input()
+if(x%4==0):
+            print("the year is leap")
+elif(x%100==0):
+	print("the year is leap")
+elif(x%400==0):
+	print("the year is leap")
+else:
+	print("the year is not leap")
